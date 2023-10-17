256b02ecc8311b321cf159e812f53a86f347f6a9
Vamsi <vamsigrandhi809@gmail.com>
2023-10-17 11:24:37 +0530

    I Have Modified some changes.

--- number_guessing.py
+++ number_guessing.py
@@ -10,8 +10,8 @@ print('Lovely to meet you! ' + player_name)
 answer = input('Do You want to play guessing game with me? ')
 if answer == 'no':
   print('oh okay, maybe next time')
-elif answer == 'yes' :
-  print('Alright ' + player_name + ' I am thinking about a number between 1 and 100 and you have 10 chances to guess it ;D')
+elif answer == 'yes':
+  print('Alright ' + player_name + ' I am thinking about a number between 1 and 100 and you have 10 chances to guess it.')
   print('Lets get started')
   while 1 <= guess_chances:
     num = int(eval(input("Guess the number " )))
@@ -29,7 +29,7 @@ elif answer == 'yes' :
 
   print("GAME OVER")
   print("Number is ", n)
-  print('Thanks for playing :D')
+  print(f'Thanks for playing :{player_name}')
 
 else:
   print('sorry, I didnt get it')
